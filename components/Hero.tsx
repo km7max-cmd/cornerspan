598cebeb8e3a7afeb3c7470ee100d738c346e5f1
YARRAMSETTY DHARMA TEJA <km7max@gmail.com>
2026-08-03 22:36:57 +0530

    Add Hero component with introductory content

---
+++ components/Hero.tsx
@@ -0,0 +1,17 @@
+import Button from "./Button";
+
+export default function Hero() {
+  return (
+    <section style={{ margin: "30px 0" }}>
+      <h1>Professional Construction Calculators</h1>
+
+      <p>
+        Built for USA Contractors, Builders and Estimators.
+      </p>
+
+      <br />
+
+      <Button text="Get Started" />
+    </section>
+  );
+}
