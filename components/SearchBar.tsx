c5ee503ee87502028bea97d4f8c79b3c480eff67
YARRAMSETTY DHARMA TEJA <km7max@gmail.com>
2026-08-03 22:57:50 +0530

    Add SearchBar component for calculator search

---
+++ components/SearchBar.tsx
@@ -0,0 +1,9 @@
+export default function SearchBar() {
+  return (
+    <input
+      type="text"
+      placeholder="Search calculators..."
+      className="w-full rounded-xl border border-slate-300 p-4 outline-none focus:border-blue-600"
+    />
+  );
+}
