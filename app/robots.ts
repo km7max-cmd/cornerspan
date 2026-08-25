9f95283f5cbb76a993e165fec98e5ef6fe383f01
YARRAMSETTY DHARMA TEJA <km7max@gmail.com>
2026-08-25 14:06:35 +0530

    Fix sitemap URL in robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,13 +1,11 @@
 import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = "https://www.cornerspan.com";
-
   return {
     rules: {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: `${baseUrl}/sitemap.xml`,
+    sitemap: "https://www.cornerspan.com/sitemap.xml",
   };
 }
