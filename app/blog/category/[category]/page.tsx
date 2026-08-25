15975d9688462e94d14ef19635e527416b2b6985
YARRAMSETTY DHARMA TEJA <km7max@gmail.com>
2026-08-25 15:49:53 +0530

    Fix import path for blogPosts data

--- app/blog/category/[category]/page.tsx
+++ app/blog/category/[category]/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { notFound } from "next/navigation";
 import type { Metadata } from "next";
-import { blogPosts } from "../../../../../data/blog";
+import { blogPosts } from "../../../../data/blog";
 
 type Props = {
   params: Promise<{ category: string }>;
